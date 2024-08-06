3c50e00107679d570bfa1dbe613f98375f7125ac
Devon Rismay <drismay@energyogre.com>
2024-08-06 17:54:14 -0500

    First test Search feature complete

--- SkillsShowcase/SkillsShowcase/Components/Pages/DcVillainsManagement.razor.cs
+++ SkillsShowcase/SkillsShowcase/Components/Pages/DcVillainsManagement.razor.cs
@@ -10,6 +10,24 @@ namespace SkillsShowcase.Components.Pages
         private GetApiClient GetDcVillainsAPIs { get; set; } = default!;
         [Parameter]
         public List<DcVillainsForApiCall>? DcVillainsForMainPage { get; set; }
+        [Parameter]
+        public List<string>? DcVillainsNameList { get; set; }
+
+        public string _searchText = "";
+        public string SearchText
+        {
+            get => _searchText;
+            set
+            {
+                if (_searchText != value)
+                {
+                    _searchText = value;
+                    FilterPlans();
+                }
+            }
+        }
+
+        public bool HasMatchingVillains { get; set; } = true;
         protected async override Task OnInitializedAsync()
         {
             await GetApiDcVillainsForView();
@@ -17,6 +35,17 @@ namespace SkillsShowcase.Components.Pages
         private async Task GetApiDcVillainsForView()
         {
             DcVillainsForMainPage = await GetDcVillainsAPIs.GetApiDcVillainsTable();
+            FilterPlans();
+        }
+        public async void FilterPlans()
+        {
+            DcVillainsNameList = DcVillainsForMainPage
+                .Where(villains => villains.VillanName.ToLower()
+                .Contains(SearchText.ToLower()))
+                .Select(villains => villains.VillanName)
+                .ToList();
+
+            HasMatchingVillains = DcVillainsNameList.Any(villainName => villainName.Equals(SearchText, StringComparison.OrdinalIgnoreCase));
         }
     }
 }
---
+++ SkillsShowcase/SkillsShowcase/Components/Pages/LayeredPages/HorizontalSlider.razor.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace SkillsShowcase.Components.Pages.LayeredPages
+{
+    public partial class HorizontalSlider : ComponentBase
+    {
+        [Parameter]
+        public RenderFragment ChildContent { get; set; }
+    }
+}
