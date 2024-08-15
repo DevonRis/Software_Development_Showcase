050545566f9c2c1061da414355070644ff5af0ce
Devon Rismay <drismay@energyogre.com>
2024-08-15 18:29:42 -0500

    Domain Models & Enums for next Mini Project

---
+++ SkillsShowcase/SkillsShowcase.Shared/Domain/Models/Enums/GuitarOptions.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel;
+
+namespace SkillsShowcase.Shared.Domain.Models.Enums
+{
+    public enum GuitarOptions
+    {
+        [Description("")]
+        NotServed = 0,
+        [Description("Fender")]
+        Fender = 1,
+        [Description("Gibson")]
+        Gibson = 2,
+        [Description("Paul Reed Smith")]
+        PaulReedSmith = 3,
+        [Description("Ibanez")]
+        Ibanez = 4
+    }
+}
---
+++ SkillsShowcase/SkillsShowcase.Shared/Domain/Models/MethodExtensions/EnumExtensionMethods.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.ComponentModel;
+using System.Reflection;
+
+namespace SkillsShowcase.Shared.Domain.Models.MethodExtensions
+{
+    public static class EnumExtensionMethods
+    {
+        public static string GetEnumDescription(this Enum enumValue)
+        {
+            FieldInfo fieldInfo = enumValue.GetType().GetField(enumValue.ToString());
+
+            DescriptionAttribute[] attributes = (DescriptionAttribute[])fieldInfo
+            .GetCustomAttributes(typeof(DescriptionAttribute), false);
+
+            if (attributes != null && attributes.Length > 0)
+                return attributes[0].Description;
+            else
+                return enumValue.ToString();
+        }
+    }
+}
