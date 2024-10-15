24c98a1cf813519e7118fcea3b974400ae202be1
Devon Rismay <drismay@energyogre.com>
2024-10-15 16:44:40 -0500

    micro changes

---
+++ SkillsShowcase/SkillsShowcase/Components/Pages/LayeredPages/NarutoInfoSubComponents/NarutoInfoAccordion.razor.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Components;
+using SkillsShowcase.Shared.Domain.Clients;
+using SkillsShowcase.Shared.Domain.Models.ApiModelsForApiCall;
+
+namespace SkillsShowcase.Components.Pages.LayeredPages.NarutoInfoSubComponents
+{
+    public partial class NarutoInfoAccordion : ComponentBase
+    {
+        [Inject]
+        private GetApiClient ApiClient { get; set; } = default!;
+        [Parameter]
+        public NarutoInfoForApiCall[] NarutoInfoDataAccordion { get; set; } = [];
+        protected async override Task OnInitializedAsync()
+        {
+            await GetNarutoInfoData();
+        }
+        private async Task GetNarutoInfoData()
+        {
+            var response = await ApiClient.GetNarutoInfo();
+            NarutoInfoDataAccordion = response!.ToArray();
+        }
+    }
+}
---
+++ SkillsShowcase/SkillsShowcase/Components/Pages/NarutoInfo.razor.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Components;
+using SkillsShowcase.Shared.Domain.Clients;
+using SkillsShowcase.Shared.Domain.Models.ApiModelsForApiCall;
+
+namespace SkillsShowcase.Components.Pages
+{
+    public partial class NarutoInfo : ComponentBase
+    {
+        [Inject]
+        private GetApiClient ApiClient { get; set; } = default!;
+        [Parameter]
+        public NarutoInfoForApiCall[] NarutoInfoData { get; set; } = [];
+        protected async override Task OnInitializedAsync() 
+        {
+            await GetNarutoInfoData();
+        }
+        private async Task GetNarutoInfoData()
+        {
+            var response = await ApiClient.GetNarutoInfo();
+            NarutoInfoData = response!.ToArray();
+        }
+    }
+}
