81a8f4a41af5975102ee297451d67010330d63f5
Devon Rismay <drismay@energyogre.com>
2025-05-16 20:04:28 -0500

    New enums

---
+++ SkillsShowcase/SkillsShowcase.Shared/Domain/Models/Enums/Occupations.cs
@@ -0,0 +1,55 @@
+﻿using System.ComponentModel;
+
+namespace SkillsShowcase.Shared.Domain.Models.Enums
+{
+    public enum Occupations
+    {
+        [Description("")]
+        NoOccupation,
+
+        [Description("Software Developer")]
+        SoftwareDeveloper,
+
+        [Description("Registered Nurse")]
+        RegisteredNurse,
+
+        [Description("Electrician")]
+        Electrician,
+
+        [Description("Teacher")]
+        Teacher,
+
+        [Description("Plumber")]
+        Plumber,
+
+        [Description("Police Officer")]
+        PoliceOfficer,
+
+        [Description("Administrative Assistant")]
+        AdministrativeAssistant,
+
+        [Description("Construction Worker")]
+        ConstructionWorker,
+
+        [Description("Truck Driver")]
+        TruckDriver,
+
+        [Description("Marketing Manager")]
+        MarketingManager,
+
+        [Description("HVAC Technician")]
+        HvacTechnician,
+
+        [Description("Graphic Designer")]
+        GraphicDesigner,
+
+        [Description("Financial Analyst")]
+        FinancialAnalyst,
+
+        [Description("Customer Service Representative")]
+        CustomerServiceRep,
+
+        [Description("Freelance Writer")]
+        FreelanceWriter
+    }
+}
