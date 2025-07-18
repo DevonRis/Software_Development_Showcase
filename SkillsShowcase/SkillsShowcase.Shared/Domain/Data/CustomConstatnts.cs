d628ddeb9fd3d1b86de7acad0e308f8f2e6b0ab1
Devon Rismay <drismay@energyogre.com>
2025-07-18 17:46:23 -0500

    New Code

--- SkillsShowcase/SkillsShowcase.Shared/Domain/Data/CustomConstatnts.cs
+++ SkillsShowcase/SkillsShowcase.Shared/Domain/Data/CustomConstatnts.cs
@@ -12,5 +12,41 @@
         public const decimal MarriedAsianHouseHolds_HS = 0.55m;
         public const decimal MarriedBlackHouseHolds_HS = 0.26m;
         public const decimal MarriedHispanicHouseHolds_HS = 0.27m;
+
+        //String Array constants
+        public static readonly string[] TopDegrees_Asians = new[]
+        {
+            "BS in Biology",
+            "BS in Computer Science",
+            "BS in Business Administration",
+            "BS in Nursing",
+            "BS in Mechanical Engineering"
+        };
+        public static readonly string[] TopDegrees_Whites = new[]
+        {
+            "BS in Business Administration",
+            "BS in Nursing",
+            "BA in Psychology",
+            "BA in Economics",
+            "BS in Biology"
+        };
+
+        public static readonly string[] TopDegrees_Blacks = new[]
+        {
+            "BS in Nursing",
+            "BS in Business Administration",
+            "BS in Criminal Justice",
+            "BA in Psychology",
+            "BSW in Social Work"
+        };
+
+        public static readonly string[] TopDegrees_Hispanics = new[]
+        {
+            "BS in Business Administration",
+            "BS in Nursing",
+            "BA in Psychology",
+            "BA in Sociology",
+            "BS in Education"
+        };
     }
 }
